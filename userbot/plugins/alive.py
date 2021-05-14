251ba565a48dedb45d8693beac49cf54c7d4a5ca
chrisdroid1 <82050423+chrisdroid1@users.noreply.github.com>
2021-05-15 00:15:10 +0100

    Update and rename alive.py to alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -1,17 +1,21 @@
+# kangers give credit
+
 import time
 from platform import python_version
 
 from telethon import version
 
-from . import StartTime, catversion, get_readable_time, hmention, mention, reply_id
+from . import StartTime, catversion, get_readable_time, ALIVE_NAME, mention, reply_id
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Creminal"
 
 CAT_IMG = Config.ALIVE_PIC
-CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "✮ MY BOT IS RUNNING SUCCESSFULLY ✮"
-EMOJI = Config.CUSTOM_ALIVE_EMOJI or "✧✧"
+CUSTOM_ALIVE_TEXT = Config.CUSTOM_ALIVE_TEXT or "⚠️ MY CREMINAL BOT IS ALIVE ⚠️"
+EMOJI = Config.CUSTOM_ALIVE_EMOJI or "⚠️"
 
 
-@bot.on(admin_cmd(outgoing=True, pattern="alive$"))
-@bot.on(sudo_cmd(pattern="alive$", allow_sudo=True))
+@bot.on(admin_cmd(outgoing=True, pattern="salive$"))
+@bot.on(sudo_cmd(pattern="salive$", allow_sudo=True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
@@ -20,19 +24,30 @@ async def amireallyalive(alive):
     _, check_sgnirts = check_data_base_heal_th()
     if CAT_IMG:
         cat_caption = f"<b>{CUSTOM_ALIVE_TEXT}</b>\n\n"
-        cat_caption += f"<b>{EMOJI} Master : {hmention}</b>\n"
-        cat_caption += f"<b>{EMOJI} Uptime :</b> <code>{uptime}</code>\n"
+
+        cat_caption += f"☞ ** MY SYSTEM STATUS**\n"
+
         cat_caption += (
-            f"<b>{EMOJI} Python Version :</b> <code>{python_version()}</code>\n"
+            f"<b>{EMOJI}  **Telethon Version:** </b> <code>{version.__version__}</code>\n"
         )
+
         cat_caption += (
-            f"<b>{EMOJI} Telethon version :</b> <code>{version.__version__}</code>\n"
+            f"<b>{EMOJI} **Python Version:** </b> <code>{python_version()}</code>\n"
         )
+
+        cat_caption += f"<b>{EMOJI} **Uptime** : `{uptime}` \n"
+
+        cat_caption += f"<b>{EMOJI} **Database :**  </b> <code>{check_sgnirts}</code>\n"
+
         cat_caption += (
-            f"<b>{EMOJI} Catuserbot Version :</b> <code>{catversion}</code>\n"
+            f"<b>{EMOJI} **Creminal Version** : </b> <code>{catversion}</code>\n"
         )
-        cat_caption += f"<b>{EMOJI} Database :</b> <code>{check_sgnirts}</code>\n\n"
-        cat_caption += "    <a href = https://github.com/sandy1709/catuserbot><b>GoodCat</b></a> | <a href = https://github.com/Jisan09/catuserbot><b>BadCat</b></a> | <a href = https://t.me/catuserbot_support><b>Support</b></a>"
+
+        cat_caption += (
+            f"↼🤠🄱🄾🅂🅂🤠⇀\n**『 [{DEFAULTUSER}] 』**\n\n"
+        )
+
+        cat_caption += "    <a href = https://github.com/chrisdroid1/Creminal><b>🎇REPO🎇</b></a> 🔹 <a href = https://t.me/CreminalUBotSupport><b>Group</b></a> 🔹 <a href = https://t.me/CreminalUBot><b>Channel</b></a>"
         await alive.client.send_file(
             alive.chat_id,
             CAT_IMG,
@@ -46,30 +61,40 @@ async def amireallyalive(alive):
     else:
         await edit_or_reply(
             alive,
-            f"<b>{CUSTOM_ALIVE_TEXT}</b>\n\n"
-            f"<b>{EMOJI} Master : {hmention}</b>\n"
-            f"<b>{EMOJI} Uptime :</b> <code>{uptime}</code>\n"
-            f"<b>{EMOJI} Python Version :</b> <code>{python_version()}</code>\n"
+            f"{CUSTOM_ALIVE_TEXT}\n\n"
+
+            f" __↼🤠🄱🄾🅂🅂🤠⇀__\n『 [{DEFAULTUSER}] 』\n\n"
+
+            f"<b> MY SYSTEM STATUS</b>\n"
+
+            f"<b>{EMOJI}} Uptime :</b> <code>{uptime}</code>\n"
+
             f"<b>{EMOJI} Telethon version :</b> <code>{version.__version__}</code>\n"
-            f"<b>{EMOJI} Catuserbot Version :</b> <code>{catversion}</code>\n"
-            f"<b>{EMOJI} Database :</b> <code>{check_sgnirts}</code>\n\n"
-            "    <a href = https://github.com/sandy1709/catuserbot><b>GoodCat</b></a> | <a href = https://github.com/Jisan09/catuserbot><b>BadCat</b></a> | <a href = https://t.me/catuserbot_support><b>Support</b></a>",
+
+            f"<b>{EMOJI} Creminal Version :</b> <code>{catversion}</code>\n"
+
+            f"<b>{EMOJI} Database :</b> <code>{check_sgnirts}</code>\n"    
+    
+            f"<b>{EMOJI} Python Version: </b> <code>{python_version()}</code>\n\n"
+
+            "    <a href = https://github.com/chrisdroid1/Creminal><b>🎇REPO🎇</b></a> 🔹 <a href = https://t.me/CreminalUBotSupport><b>Group</b></a> 🔹 <a href = https://t.me/CreminalUBot><b>Channel</b></a>",
             parse_mode="html",
         )
-
-
-@bot.on(admin_cmd(outgoing=True, pattern="ialive$"))
-@bot.on(sudo_cmd(pattern="ialive$", allow_sudo=True))
+        
+@bot.on(admin_cmd(outgoing=True, pattern="sialive$"))
+@bot.on(sudo_cmd(pattern="sialive$", allow_sudo=True))
 async def amireallyalive(alive):
     if alive.fwd_from:
         return
     tgbotusername = Config.TG_BOT_USERNAME
     reply_to_id = await reply_id(alive)
-    cat_caption = f"**Catuserbot is Up and Running**\n"
-    cat_caption += f"**  -Master :** {mention}\n"
-    cat_caption += f"**  -Python Version :** `{python_version()}\n`"
+    cat_caption = f"**Creminal is Up and Running**\n"
+
+    cat_caption += f"**  -Boss :** {mention}\n"
+
     cat_caption += f"**  -Telethon version :** `{version.__version__}\n`"
-    cat_caption += f"**  -Catuserbot Version :** `{catversion}`\n"
+
+    cat_caption += f"**  -Creminal Version :** `{catversion}`\n"
     results = await bot.inline_query(tgbotusername, cat_caption)  # pylint:disable=E0602
     await results[0].click(alive.chat_id, reply_to=reply_to_id, hide_via=True)
     await alive.delete()
@@ -109,9 +134,9 @@ def check_data_base_heal_th():
 CMD_HELP.update(
     {
         "alive": "__**PLUGIN NAME :** Alive__\
-      \n\n📌** CMD ➥** `.alive`\
+      \n\n📌** CMD ➥** `.salive`\
       \n**USAGE   ➥  **To see wether your bot is working or not.\
-      \n\n📌** CMD ➥** `.ialive`\
+      \n\n📌** CMD ➥** `.sialive`\
       \n**USAGE   ➥  **__Status of bot will be showed by inline mode with button__."
     }
 )
